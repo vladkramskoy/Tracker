ce24aeef2f06819aff2d11d24635cd90bea63304
Vlad Kramskoy <vkto5609@gmail.com>
2024-06-14 16:45:36 +0300

    Add structures: TrackerCategory, TrackerRecord

---
+++ Tracker/Models/TrackerCategory.swift
@@ -0,0 +1,13 @@
+//
+//  TrackerCategory.swift
+//  Tracker
+//
+//  Created by Vladislav Kramskoy on 14.06.2024.
+//
+
+import Foundation
+
+struct TrackerCategory {
+    let name: String
+    let trackers: [Tracker]
+}
