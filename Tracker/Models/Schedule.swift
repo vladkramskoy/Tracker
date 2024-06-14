d67d619b64a7b5cc5230af8f6921e292233ae05d
Vlad Kramskoy <vkto5609@gmail.com>
2024-06-14 15:39:18 +0300

    Add structures: Tracker, Schedule

---
+++ Tracker/Models/Schedule.swift
@@ -0,0 +1,18 @@
+//
+//  Schedule.swift
+//  Tracker
+//
+//  Created by Vladislav Kramskoy on 14.06.2024.
+//
+
+import Foundation
+
+struct Schedule {
+    let monday: Bool
+    let tuesday: Bool
+    let wednesday: Bool
+    let thursday: Bool
+    let friday: Bool
+    let saturday: Bool
+    let sunday: Bool
+}
